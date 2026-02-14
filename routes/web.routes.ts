b824e7a4a83b5cd91a6d22754c480c511332f6ea
shyy23 <syahrulhidayatulloh14@gmail.com>
2026-02-14 11:59:45 +0700

    add bug

--- routes/web.routes.ts
+++ routes/web.routes.ts
@@ -7,4 +7,5 @@ const schoolController = new SchoolController()
 web.get('/schools', schoolController.getListData)
 web.get('/school/:id', schoolController.getDetailData)
 
+web.get('/schools/:id', schoolController.getDetailData)
 export default web
